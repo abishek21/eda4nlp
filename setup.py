e0adc0183ada8b4c7279fd9bf857fd1ea58eb567
abishek21 <32140923+abishek21@users.noreply.github.com>
2020-05-24 20:18:09 +0530

    embeddings1

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@ from distutils.core import setup
 setup(
   name = 'eda4nlp',         # How you named your package folder (MyLib)
   packages = ['eda4nlp'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'Abishek Kamalanathan',                   # Type in your name
   author_email = 'neyveliabi@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/abishek21',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/abishek21/eda4nlp/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/abishek21/eda4nlp/archive/0.3.tar.gz',    # I explain this later on
   keywords = ['nlp', 'ngrams', 'eda'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
           'sklearn',
-          'matplotlib'
+          'matplotlib',
+          'numpy',
+          'os'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
