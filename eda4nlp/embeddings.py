d4ee47bad8b5334c369eb84ee1464e1eeffa653b
abishek21 <32140923+abishek21@users.noreply.github.com>
2020-05-24 20:00:15 +0530

    embeddings

---
+++ eda4nlp/embeddings.py
@@ -0,0 +1,21 @@
+import numpy as np
+import os
+
+def get_embeddings(word_index,max_words,glove_dir,glove_path,embedding_dim):
+    embeddings_index = {}
+    f = open(os.path.join(glove_dir,glove_path),encoding="utf8")
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = np.asarray(values[1:], dtype='float32')
+        embeddings_index[word] = coefs
+    f.close()
+    #print('Found %s word vectors.' % len(embeddings_index))
+
+    embedding_matrix = np.zeros((max_words, embedding_dim))
+    for word, i in word_index.items():
+        if i < max_words:
+            embedding_vector = embeddings_index.get(word)
+            if embedding_vector is not None:
+                embedding_matrix[i] = embedding_vector
+    return embedding_matrix
