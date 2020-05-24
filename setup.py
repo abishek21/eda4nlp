fc76bad3e4ff9c15a5b8fc1fa6b1f093af445af3
abishek21 <32140923+abishek21@users.noreply.github.com>
2020-05-24 23:01:15 +0530

    setup commit

--- setup.py
+++ setup.py
@@ -9,14 +9,13 @@ setup(
   author_email = 'neyveliabi@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/abishek21',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/abishek21/eda4nlp/archive/0.3.tar.gz',    # I explain this later on
-  keywords = ['nlp', 'ngrams', 'eda'],   # Keywords that define your package best
+  keywords = ['nlp', 'ngrams', 'eda','embeddings'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
           'sklearn',
           'matplotlib',
-          'numpy',
-          'os'
-      ],
+          'numpy'
+          ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
